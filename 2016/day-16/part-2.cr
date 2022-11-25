b001635729715d0a80d7883cef432d7bd5fbd7ef
Andy Paine <andy.paine@numan.com>
2022-11-25 22:54:35 +0000

    2016: 16-1+2

---
+++ 2016/day-16/part-2.cr
@@ -0,0 +1,13 @@
+INPUT = STDIN.gets_to_end.strip.chars.map(&.to_i)
+LENGTH = 35651584
+
+rounds = (Math.log2(LENGTH + 1) - Math.log2(INPUT.size) - 1).ceil.to_i
+data = rounds.times.reduce(INPUT) {|a| a + [0] + a.reverse.map(&.^ 1) }.first(LENGTH)
+
+def checksum(data)
+  chk = data.each_slice(2).map{|(a,b)| a ^ b ^ 1}.to_a
+  return chk if chk.size.odd?
+  checksum(chk)
+end
+
+puts checksum(data).join
